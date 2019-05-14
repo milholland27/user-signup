fe02dfd9d9bbb0ba16794cf832929520f4833033
Dan Milholland <milholland@mac.com>
2019-05-14 14:06:44 -0500

    dog

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/form-inputs")
-def display_form_inputs():
-    return index.html
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route("/form-inputs", methods=['POST'])
-def print_form_values():
-    resp = ""
+@app.route("/", methods=['POST'])
+def user_signup():
+    resp = ("")
     for field in request.form.keys():
         resp += "<b>{key}</b>: {value}<br>".format(key=field, value=request.form[field])
 
